0f6db81a7a2327c1dbed3645b84a8b3ce47e8364
shaundavies <57547901+shaundavies@users.noreply.github.com>
2020-01-31 11:48:52 -0800

    massive update

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -9,10 +9,16 @@ import { Router } from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   games: []
-  constructor(private _httpService: HttpService, private router: Router) {}
+
+  slideIndex = 1;
+  count = 0;
+
+  allGames = [];
+  constructor(private _httpService: HttpService, private router: Router) { }
 
   ngOnInit() {
     this.getAllGames();
+    this.test();
   }
 
   getAllGames() {
@@ -21,5 +27,121 @@ export class HomeComponent implements OnInit {
       console.log("Got our games data!", data);
       this.games = data["items"];
     });
+  }
+
+
+
+
+
+
+
+
+  plusSlides(n) {
+
+    console.log("n value: ", n)
+
+
+    this.slideIndex += n
+
+    console.log("slideindex value: ", this.slideIndex)
+    // if (this.slideIndex < 1) {
+    //   this.slideIndex = 3;
+    // }
+    if (this.slideIndex > 3) {
+      this.slideIndex = 1
+    }
+
+
+    // n++;
+    // n = n + 1;
+    // console.log("slideindex value: ", this.slideIndex)
+    // var slideIndex = 1;
+    // this.slideIndex = n;
+    // console.log("this.slideindex value: ", this.slideIndex)
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { this.slideIndex = n }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) { // this is how you tell typescript to ignore/shutup the errors
+      // @ts-ignore
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    // @ts-ignore
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+    // showSlides(this.slideIndex += n);
+  }
+
+
+
+  test() {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // function plusSlides(n) {
+    //   var i;
+    //   var slides = document.getElementsByClassName("mySlides");
+    //   var dots = document.getElementsByClassName("dot");
+    //   if (n > slides.length) { slideIndex += n }
+    //   if (n < 1) { slideIndex = slides.length }
+    //   for (i = 0; i < slides.length; i++) { // this is how you tell typescript to ignore/shutup the errors
+    //     // @ts-ignore
+    //     slides[i].style.display = "none";
+    //   }
+    //   for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    //   }
+    //   // @ts-ignore
+    //   slides[slideIndex - 1].style.display = "block";
+    //   dots[slideIndex - 1].className += " active";
+    //   // showSlides(slideIndex += n);
+    // }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+      for (i = 0; i < slides.length; i++) { // this is how you tell typescript to ignore/shutup the errors
+        // @ts-ignore
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      // @ts-ignore
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
+
+    var slideIndex = 0;
+    showSlidesOne();
+
+    function showSlidesOne() {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        // @ts-ignore
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) { slideIndex = 1 }
+      // @ts-ignore
+      slides[slideIndex - 1].style.display = "block";
+
+      dots[slideIndex - 1].className += " active";
+      setTimeout(showSlidesOne, 9000);
+    }
   }
 }
